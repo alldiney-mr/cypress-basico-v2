71f8825aae09c47bfa077c0061af42ede2de6c43
Aldiney Moreira <alldiney.mr@gmail.com>
2022-06-22 17:04:25 -0300

    Exercício extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
    })
 
-   it.only('preenche os campos obrigatórios e envia o formulário', function() {
+   it('preenche os campos obrigatórios e envia o formulário', function() {
       const textLong = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor augue lorem, eu bibendum mauris blandit et. Vivamus eu scelerisque purus. Proin quis libero consequat, tempus nisl in, lacinia augue. Nunc eros augue, rutrum quis pretium ut, molestie sed turpis. Integer eget nibh dolor. Praesent iaculis ac ante sed luctus. Sed rhoncus accumsan cursus. Praesent hendrerit in tellus vel iaculis. Aenean ut condimentum sapien. Suspendisse mauris nisl, dignissim id porta ac, venenatis id sapien. Suspendisse at suscipit neque, vel rhoncus lacus. Mauris sem dolor, fermentum quis congue scelerisque, condimentum vel ex. Sed ut ex rhoncus, porttitor ipsum vitae, pellentesque dolor.'
       cy.get('#firstName').type('Aldiney')
       cy.get('#lastName').type('Ribeiro')
@@ -18,6 +18,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('button[type="submit"]').click()
 
       cy.get('.success').should('be.visible')
+   })
+
+   it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+      cy.get('#firstName').type('Aldiney')
+      cy.get('#lastName').type('Ribeiro')
+      cy.get('#email').type('aldiney.ribeiro@gmail,com')
+      cy.get('#open-text-area').type('teste')
+      cy.get('button[type="submit"]').click()
+
+      cy.get('.error').should('be.visible')
    })
 })
 
