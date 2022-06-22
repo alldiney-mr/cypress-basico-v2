4eea849577da63adda74f1173041694763fc0797
Aldiney Moreira <alldiney.mr@gmail.com>
2022-06-22 16:58:26 -0300

    Exercício extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,10 +10,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
    })
 
    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+      const textLong = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor augue lorem, eu bibendum mauris blandit et. Vivamus eu scelerisque purus. Proin quis libero consequat, tempus nisl in, lacinia augue. Nunc eros augue, rutrum quis pretium ut, molestie sed turpis. Integer eget nibh dolor. Praesent iaculis ac ante sed luctus. Sed rhoncus accumsan cursus. Praesent hendrerit in tellus vel iaculis. Aenean ut condimentum sapien. Suspendisse mauris nisl, dignissim id porta ac, venenatis id sapien. Suspendisse at suscipit neque, vel rhoncus lacus. Mauris sem dolor, fermentum quis congue scelerisque, condimentum vel ex. Sed ut ex rhoncus, porttitor ipsum vitae, pellentesque dolor.'
       cy.get('#firstName').type('Aldiney')
       cy.get('#lastName').type('Ribeiro')
       cy.get('#email').type('aldiney.ribeiro@gmail.com')
-      cy.get('#open-text-area').type('Teste')
+      cy.get('#open-text-area').type(textLong, { delay: 0 })
       cy.get('button[type="submit"]').click()
 
       cy.get('.success').should('be.visible')
