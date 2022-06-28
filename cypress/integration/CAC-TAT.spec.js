84c84a3e5e7a3cd6e423b5f56ab6a5ad3d7e676a
Aldiney Moreira <alldiney.mr@gmail.com>
2022-06-28 15:08:31 -0300

    Exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -104,13 +104,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
          .should('have.value', 'feedback').should('be.checked')
    })
 
-   it.only('marca cada tipo de atendimento', function() {
+   it('marca cada tipo de atendimento', function() {
       cy.get('input[type="radio"]')
          .should('have.length', 3)
          .each(function($radio){
             cy.wrap($radio).check()
             cy.wrap($radio).should('be.checked')
          })
+   })
+
+   it.only('marca ambos checkboxes, depois desmarca o último', function() {
+      cy.get('input[type="checkbox"]')
+         .check()
+         .should('be.checked')
+         .last()
+         .uncheck()
+         .should('not.be.checked')
    })
 })
 
