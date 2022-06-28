891f4b50aaebe11938f905e4f9d0988464db4c25
Aldiney Moreira <alldiney.mr@gmail.com>
2022-06-28 19:15:25 -0300

    Exercício extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -122,13 +122,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
          .should('not.be.checked')
    })
 
-   it.only('seleciona um arquivo da pasta fixtures', function() {
+   it('seleciona um arquivo da pasta fixtures', function() {
       cy.get('input[type="file"]#file-upload')
          .should('not.have.value')
          .selectFile('cypress/fixtures/example.json')
          .should(function($input) {
             expect($input[0].files[0].name).to.equal('example.json')
          })
+   })
+
+   it.only('seleciona um arquivo simulando um drag-and-drop', function() {
+      cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/example.json')
+      .should(function($input) {
+         expect($input[0].files[0].name).to.equal('example.json', { action: 'drag-drop' })
+      })
    })
 })
 
