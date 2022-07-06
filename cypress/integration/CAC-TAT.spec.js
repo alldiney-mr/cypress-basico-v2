8d2000820681236e3fa91709e04dbf0597ceef61
Aldiney Moreira <alldiney.mr@gmail.com>
2022-07-06 18:20:03 -0300

    Exercício extra 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -200,12 +200,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('not.be.visible')
     })
 
-    it.only('preenche a area de texto usando o comando invoke', function() {
+    it('preenche a area de texto usando o comando invoke', function() {
       const longText = Cypress._.repeat('teste ', 20)
 
       cy.get('#open-text-area')
          .invoke('val', longText)
          .should('have.value', longText)
+    })
+
+    it('faz uma requisição HTTP', function() {
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+         .should(function(response) {
+            const { status, statusText, body } = response;
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+         })
     })
 })
 
